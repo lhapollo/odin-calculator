50f6bfb94857d00a2a81603effd93e2df550ae31
Lexi Han <hanlexi@gmail.com>
2025-01-02 17:24:38 -0500

    Created Files

---
+++ script.js
@@ -0,0 +1,30 @@
+var num1, num2, op;
+
+const add = (a,b) => a + b;
+const subtract = (a,b) => a - b;
+const multiply = (a,b) => a * b;
+const divide = (a,b) => {
+    if (b === 0) return "ERROR";
+    else return parseFloat((a/b).toFixed(5));
+}
+
+const operate = () => {
+    switch (op){
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+    }
+}
+
+/*
+TO DO:
+- html calculator 
+- create functions that populate display
+- make calculator work
+- gotchas
+*/
